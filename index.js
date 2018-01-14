68f95d11d80397b3c707eb0d62d84c697f3dcf9c
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-01-14 21:34:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function getFirstSelector(selector) {
+  const firstSelector = document.querySelector(selector);
+  return firstSelector
+}
+
+function nestedTarget() {
+  const target = document.querySelector('#nested').querySelector('.target');
+  return target
+}
+
+function deepestChild() {
+  let currentChild = array;
+  let next = [];
+  const deepest = document.querySelector('#grand-node').querySelectorAll('currentChild');
+
+  while (currentChild) {
+    if (currentChild != null){
+      return currentChild;
+    }
+
+    if (Array.isArray(currentChild)){
+      for (let i=0; i < currentChild.length; i++) {
+        next.push(currentChild[i])
+      }
+
+      currentChild = next.shift();
+
+    }
+
+    return null
+
+  }
+
+}
+
+// function increaseRankBy(n) {
+//   const increaseRank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+//
+//   for (let i = 0; i < increaseRank.length; i++) {
+//     return increaseRank[i].innerHTML = parseInt(i + n);
+//   }
+//
+// }
